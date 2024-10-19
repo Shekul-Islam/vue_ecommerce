d6e58ddca7bcd0912bc7368b6a70fcd0c7e33727
Shekul-Islam <shekul.bd201@gmail.com>
2024-10-19 18:59:03 +0600

    udpated

--- src/router/Index.js
+++ src/router/Index.js
@@ -2,24 +2,50 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Index from "../views/pages/Index.vue";
 import Shop from "../views/pages/shop/Shop.vue";
-import Seller from "../views/pages/seller/Seller.vue";
+import { SellerPage, SellerList } from "@/views/pages/seller";
 import WishList from "../views/pages/wishlist/WishList.vue";
-import Login from "../views/pages/Login.vue";
-import Register from "../views/pages/Register.vue";
+import { Login, Register } from "@/views/auth";
 
 const routes = [
-  { path: "/", name: "index", component: Index },
-  { path: "/shop", name: "shop", component: Shop },
-  { path: "/seller", name: "seller", component: Seller },
-  { path: "/wishlist", name: "wishlist", component: WishList },
+  { path: "/", name: "index", component: Index, meta: { title: "Home" } },
+  { path: "/shop", name: "shop", component: Shop, meta: { title: "Shop" } },
+  {
+    path: "/seller-page",
+    name: "seller-page",
+    component: SellerPage,
+    meta: { title: "Seller-Page" },
+  },
+  {
+    path: "/seller-list",
+    name: "seller-list",
+    component: SellerList,
+    meta: { title: "Seller-List" },
+  },
+  {
+    path: "/wishlist",
+    name: "wishlist",
+    component: WishList,
+    meta: { title: "Wish-List" },
+  },
   { path: "/login", name: "login", component: Login },
-  { path: "/register", name: "register", component: Register, meta: { title: "Nittonoton | Profile", albab: true },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+    meta: { title: "Register", shekul: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+const defaul_title = "404";
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || defaul_title;
+  next();
 });
 
 export default router;
---
+++ src/views/pages/seller/index.js
@@ -0,0 +1,2 @@
+export { default as SellerList }  from "./SellerList.vue";
+export { default as SellerPage }  from "./SellerPage.vue";
